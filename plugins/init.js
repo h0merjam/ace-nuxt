77cfb4e43b167a869076b2a8e8059c403c3f9a88
James Homer <james@homerjam.es>
2020-02-26 14:28:10 +0000

    trigger init hooks immediatley in spa mode

--- plugins/init.js
+++ plugins/init.js
@@ -1,13 +1,21 @@
-export default (context, inject) => {
+export default ({ app }, inject) => {
   const hooks = [];
 
   const init = async hook => {
     if (hook) {
       hooks.push(hook);
+
+      // Execute hook immediately in `spa` mode
+      if (!app.context.nuxtState) {
+        await hook();
+      }
+
       return;
     }
 
     await Promise.all(hooks.map(hook => hook()));
+
+    return;
   };
 
   inject('init', init);
